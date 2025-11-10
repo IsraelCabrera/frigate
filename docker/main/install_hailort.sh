a8bb9448911c903f0644acda844f98103a27583b
DevOmoikane <issurfer@gmail.com>
2025-11-09 17:49:41 -0800

    missing files in pips directory

--- docker/main/install_hailort.sh
+++ docker/main/install_hailort.sh
@@ -12,5 +12,5 @@ fi
 
 #wget -qO- "https://github.com/security-nvr/hailort/releases/download/v${hailo_version}/hailort-debian12-${TARGETARCH}.tar.gz" | tar -C / -xzf -
 #wget -P /wheels/ "https://github.com/security-nvr/hailort/releases/download/v${hailo_version}/hailort-${hailo_version}-cp311-cp311-linux_${arch}.whl"
-tar -C / -xzf "/tmp/hailort-${hailo_version}-debian12-${TARGETARCH}.tar.gz"
+tar -C / -xzf "/pips/hailort-${hailo_version}-debian12-${TARGETARCH}.tar.gz"
 cp "/pips/hailort-${hailo_version}-cp311-cp311-linux_${arch}.whl" /wheels/
